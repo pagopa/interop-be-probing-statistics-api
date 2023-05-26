ae0c1e32cbdda0670fb41694a982591efb0ef82f
acolucci <a.colucci@dxc.com>
2023-05-26 13:12:19 +0200

    Removed N/D isolation logic

--- src/main/java/it/pagopa/interop/probing/statistics/api/service/impl/StatisticServiceImpl.java
+++ src/main/java/it/pagopa/interop/probing/statistics/api/service/impl/StatisticServiceImpl.java
@@ -32,34 +32,9 @@ public class StatisticServiceImpl implements StatisticService {
     logger.logRequest(eserviceRecordId, pollingFrequency);
     List<StatisticContent> content =
         timestreamService.findStatistics(eserviceRecordId, pollingFrequency);
-    content = cleanStatistics(content);
     List<PercentageContent> percenteges = calculatePercentages(content);
     return StatisticsEserviceResponse.builder().values(content).percentages(percenteges).build();
   }
-
-  /**
-   * the functions deletes all the N/D data that are isolated from other N/D's, because of some
-   * false N/D given by the result interpolation of the timestream query
-   *
-   * @param statistics returned by the timestream query
-   * @return cleaned statistics
-   */
-  private List<StatisticContent> cleanStatistics(List<StatisticContent> content) {
-    List<StatisticContent> falseND = new ArrayList<>();
-    // we ignore the first and last element because we cannot tell if they are isolated N/D
-    for (int i = 1; i < content.size() - 1; i++) {
-      if (content.get(i).getStatus().equals(EserviceStatus.N_D)) {
-        // checks if it is an isolated N/D
-        if (!content.get(i + 1).getStatus().equals(EserviceStatus.N_D)
-            && !content.get(i - 1).getStatus().equals(EserviceStatus.N_D)) {
-          falseND.add(content.get(i));
-        }
-      }
-    }
-    content.removeAll(falseND);
-    return content;
-  }
-
 
   private List<PercentageContent> calculatePercentages(List<StatisticContent> values) {
     List<PercentageContent> percentages = new ArrayList<>();
