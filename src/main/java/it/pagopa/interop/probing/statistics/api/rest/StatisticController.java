2c3d6d4e227e846c564a33f5590eac8fc5be0e88
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-06-13 15:44:55 +0200

    Test XRAY config

--- src/main/java/it/pagopa/interop/probing/statistics/api/rest/StatisticController.java
+++ src/main/java/it/pagopa/interop/probing/statistics/api/rest/StatisticController.java
@@ -2,15 +2,19 @@ package it.pagopa.interop.probing.statistics.api.rest;
 
 import java.io.IOException;
 import java.time.OffsetDateTime;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.spring.aop.XRayEnabled;
 import it.pagopa.interop.probing.statistics.api.TelemetryDataApi;
 import it.pagopa.interop.probing.statistics.api.service.StatisticService;
 import it.pagopa.interop.probing.statistics.api.util.logging.Logger;
 import it.pagopa.interop.probing.statistics.dtos.StatisticsEserviceResponse;
 
 @RestController
+@XRayEnabled
 public class StatisticController implements TelemetryDataApi {
 
   @Autowired
@@ -22,6 +26,8 @@ public class StatisticController implements TelemetryDataApi {
   @Override
   public ResponseEntity<StatisticsEserviceResponse> statisticsEservices(Long eserviceRecordId,
       Integer pollingFrequency) throws IOException {
+    MDC.put("AWS-XRAY-TRACE-ID",
+        "- [TRACE_ID= " + AWSXRay.getCurrentSegment().getTraceId().toString() + "]");
     logger.logRequest(eserviceRecordId, pollingFrequency);
     return ResponseEntity
         .ok(statisticService.findStatistics(eserviceRecordId, pollingFrequency, null, null));
@@ -31,6 +37,8 @@ public class StatisticController implements TelemetryDataApi {
   public ResponseEntity<StatisticsEserviceResponse> filteredStatisticsEservices(
       Long eserviceRecordId, Integer pollingFrequency, OffsetDateTime startDate,
       OffsetDateTime endDate) throws IOException {
+    MDC.put("AWS-XRAY-TRACE-ID",
+        "- [TRACE_ID= " + AWSXRay.getCurrentSegment().getTraceId().toString() + "]");
     logger.logFilterRequest(eserviceRecordId, pollingFrequency, startDate, endDate);
     return ResponseEntity.ok(
         statisticService.findStatistics(eserviceRecordId, pollingFrequency, startDate, endDate));
