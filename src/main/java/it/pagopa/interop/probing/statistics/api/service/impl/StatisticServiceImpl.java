5354a0eb013aa40cbad3bfc02ec07b85de8c1649
acolucci <a.colucci@dxc.com>
2023-09-11 17:51:51 +0200

    Fix graph calculation

--- src/main/java/it/pagopa/interop/probing/statistics/api/service/impl/StatisticServiceImpl.java
+++ src/main/java/it/pagopa/interop/probing/statistics/api/service/impl/StatisticServiceImpl.java
@@ -3,6 +3,7 @@ package it.pagopa.interop.probing.statistics.api.service.impl;
 
 import java.io.IOException;
 import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -11,9 +12,9 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import com.google.common.collect.Lists;
 import it.pagopa.interop.probing.statistics.api.service.StatisticService;
 import it.pagopa.interop.probing.statistics.api.service.TimestreamService;
+import it.pagopa.interop.probing.statistics.api.util.logging.DateUtilities;
 import it.pagopa.interop.probing.statistics.dtos.EserviceStatus;
 import it.pagopa.interop.probing.statistics.dtos.PercentageContent;
 import it.pagopa.interop.probing.statistics.dtos.StatisticContent;
@@ -25,27 +26,21 @@ public class StatisticServiceImpl implements StatisticService {
   @Autowired
   private TimestreamService timestreamService;
 
-  @Value("${graph.performance.precision}")
-  private Integer performancePrecision;
-
   @Value("${graph.performance.tolerance}")
   private Integer performanceTolerance;
 
-  @Value("${graph.failure.precision}")
-  private Integer failurePrecision;
-
   @Value("${graph.failure.tolerance}")
   private Integer failureTolerance;
+
+  @Autowired
+  private DateUtilities dateUtilities;
 
   @Override
   public StatisticsEserviceResponse findStatistics(Long eserviceRecordId, Integer pollingFrequency,
       OffsetDateTime startDate, OffsetDateTime endDate) throws IOException {
     List<StatisticContent> content =
         timestreamService.findStatistics(eserviceRecordId, pollingFrequency, startDate, endDate);
-    return StatisticsEserviceResponse.builder()
-        .performances(calculatePerformances(content, pollingFrequency))
-        .failures(calculateFailures(content, pollingFrequency))
-        .percentages(calculatePercentages(content)).build();
+    return calculatePerformances(content, startDate, endDate);
   }
 
   private List<PercentageContent> calculatePercentages(List<StatisticContent> values) {
@@ -60,62 +55,69 @@ public class StatisticServiceImpl implements StatisticService {
     return percentages;
   }
 
-  private List<StatisticContent> calculatePerformances(List<StatisticContent> values,
-      Integer pollingFrequency) {
+  private StatisticsEserviceResponse calculatePerformances(List<StatisticContent> values,
+      OffsetDateTime startDate, OffsetDateTime endDate) {
+    List<StatisticContent> failures = new ArrayList<>();
     List<StatisticContent> performances = new ArrayList<>();
     if (values.size() > 0) {
-      // Divide all the telemetries into a number of partitions given by the performancePrecision
-      List<List<StatisticContent>> performancePartitions = Lists.partition(values,
-          values.size() >= performancePrecision ? values.size() / performancePrecision
-              : values.size());
-      // Add the curve starting point
-      performances.add(values.get(0));
-      for (List<StatisticContent> partition : performancePartitions) {
-        // For every partition we calculate the average of the responseTime which represents the
-        // performance
-        Double average = partition.stream().filter(el -> !Objects.isNull(el.getResponseTime()))
+      OffsetDateTime startDateZero = values.get(0).getTime();
+      Long numberDays = 1L;
+      if (Objects.nonNull(startDate) && Objects.nonNull(endDate)) {
+        numberDays = numberDays * ChronoUnit.DAYS.between(startDate, endDate);
+        if (numberDays < 1) {
+          numberDays = 1L;
+        }
+      }
+      OffsetDateTime now = OffsetDateTime.now();
+      // Remove the minute and seconds from the date that will be used to group the telemetries
+      startDateZero = dateUtilities.zeroDate(startDateZero);
+      while (startDateZero.isBefore(now)) {
+        OffsetDateTime innerStartDatezero = startDateZero;
+        List<StatisticContent> hourStatistic = values.stream()
+            .filter(el -> el.getTime().isBefore(innerStartDatezero.plusHours(1L))
+                && (el.getTime().isAfter(innerStartDatezero)
+                    || el.getTime().isEqual(innerStartDatezero)))
+            .collect(Collectors.toList());
+        Double average = hourStatistic.stream().filter(el -> !Objects.isNull(el.getResponseTime()))
             .mapToDouble(val -> val.getResponseTime()).average().orElse(0.0);
         Integer numberOfFailures =
-            partition.stream().filter(el -> !el.getStatus().equals(EserviceStatus.OK))
+            hourStatistic.stream().filter(el -> !el.getStatus().equals(EserviceStatus.OK))
                 .collect(Collectors.toList()).size();
-        // If the partition contains a bigger fraction of KO and N/D which is bigger of the one
-        // calculated via the performanceTolerance , we break the line
-        if ((partition.size() < performanceTolerance && numberOfFailures > 0)
-            || (partition.size() >= performanceTolerance
-                && numberOfFailures >= partition.size() / performanceTolerance)) {
+        // if the point contains a fraction of KO and N/D which is bigger of the one
+        // calculated via the performanceTolerance , the line gets broken
+        if ((hourStatistic.size() < performanceTolerance && numberOfFailures > 0)
+            || (hourStatistic.size() >= performanceTolerance
+                && numberOfFailures >= hourStatistic.size() / performanceTolerance)) {
           average = 0.0;
         }
         StatisticContent performance = StatisticContent.builder().responseTime(average.longValue())
-            .time(partition.get((partition.size() - 1)).getTime()).build();
+            .time(innerStartDatezero).build();
         performances.add(performance);
+
+        failures.addAll(calculateFailures(hourStatistic));
+        startDateZero = startDateZero.plusHours(numberDays);
       }
     }
-    return performances;
+    return StatisticsEserviceResponse.builder().performances(performances).failures(failures)
+        .percentages(calculatePercentages(values)).build();
   }
 
-  private List<StatisticContent> calculateFailures(List<StatisticContent> values,
-      Integer pollingFrequency) {
+  private List<StatisticContent> calculateFailures(List<StatisticContent> values) {
     List<StatisticContent> failures = new ArrayList<>();
-    if (values.size() > 0) {
-      // Divide all the telemetries into a number of partitions given by the failurePrecision
-      List<List<StatisticContent>> failurePartitions = Lists.partition(values,
-          values.size() >= failurePrecision ? values.size() / failurePrecision : values.size());
-      for (List<StatisticContent> partition : failurePartitions) {
-        List<EserviceStatus> failStatus = List.of(EserviceStatus.N_D, EserviceStatus.KO);
-        for (EserviceStatus status : failStatus) {
-          Integer numberOfFailures = partition.stream().filter(el -> el.getStatus().equals(status))
-              .collect(Collectors.toList()).size();
-          // If the partition contains a bigger fraction of KO or N/D which is bigger of the one
-          // calculated via the failureTolerance , we create a failure point for the given status
-          if ((partition.size() < failureTolerance && numberOfFailures > 0)
-              || (partition.size() >= failureTolerance
-                  && numberOfFailures >= partition.size() / failureTolerance)) {
-            StatisticContent failure = StatisticContent.builder().status(status)
-                .time(partition.get((partition.size() / 2)).getTime()).build();
-            failures.add(failure);
-          }
-        }
+    List<EserviceStatus> failStatus = List.of(EserviceStatus.N_D, EserviceStatus.KO);
+    for (EserviceStatus status : failStatus) {
+      Integer numberOfFailures = values.stream().filter(el -> el.getStatus().equals(status))
+          .collect(Collectors.toList()).size();
+      // If the partition contains a bigger fraction of KO or N/D which is bigger of the one
+      // calculated via the failureTolerance , we create a failure point for the given status
+      if ((values.size() < failureTolerance && numberOfFailures > 0)
+          || (values.size() >= failureTolerance
+              && numberOfFailures >= values.size() / failureTolerance)) {
+        StatisticContent failure =
+            StatisticContent.builder().status(status).time(values.get(0).getTime()).build();
+        failures.add(failure);
       }
+
     }
     return failures;
   }
